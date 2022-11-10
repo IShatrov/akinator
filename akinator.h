a34d9a472509e7f137f10cbe2939eed8bb65493d
IShatrov <shatrov2004@mail.ru>
2022-11-10 09:18:22 +0300

    Documentation

--- akinator.h
+++ akinator.h
@@ -23,24 +23,46 @@ const char GAME_IS_ON = 1;
 
 //akinator_game.cpp
 
+//! @param[in] filename Database filename.
+//! @brief Starts game.
 void start_game(const char *filename);
 
+//! @param[in] tree Pointer to tree.
+//! @brief Starts guess mode.
 void guess(my_tree *tree);
 
+//! @param[in] buffer Chars array.
+//! @brief Reads chars from stdin until \n is met and stores them in buffer.
 void read(char *buffer);
 
+//! @param[in] tree Pointer to tree.
+//! @param[in] bad_node Node which will be updated.
+//! @brief Updates database.
 void update_database(my_tree *tree, tree_node *bad_node);
 
+//! @param[in] node Node to start searching from.
+//! @param[in] target Target.
+//! @param[in] stk Stack to save route from node to target if found.
+//! @brief Finds target in tree.
 char tree_find(tree_node *node, const char *target, my_stk *stk);
 
+//! @param[in] tree Pointer to tree.
+//! @brief Starts definition mode.
 void def(my_tree *tree);
 
+//! @param[in] tree Pointer to tree.
+//! @brief Starts comparison mode.
 void comp(my_tree *tree);
 
+//! @param[in] text Text to say.
+//! @brief Says text using espeak.
 void aki_speak(const char *text);
 
 //akinator_read.cpp
 
+//! @param[in] filename Database filename.
+//! @param[in] tree Tree to save database at.
+//! @brief Reads database.
 void get_database(const char *filename, my_tree *tree);
 
 //! @param[in] stream File to examine.
@@ -53,16 +75,31 @@ size_t get_file_size(FILE *stream);
 //! @brief Reads text from stream and stores it as char array.
 char* read_text(FILE *stream);
 
+//! @param[in] text Text to parse.
+//! @param[in] tree Tree to save database at.
+//! @brief Parses database.
 void parse_database(const char *text, my_tree *tree);
 
+//! @param[in] text Text to read from.
+//! @param[in] buffer Buffer to save text to.
+//! @brief Reads values for nodes.
 char fill_node(const char **text, char *buffer);
 
+//! @param[in] text Text.
+//! @param[in] tree Pointer to tree.
+//! @param[in] node Node to parse.
+//! @param[in] buffer Buffer containing value of node.
+//! @brief Parses node.
 void parse_node(const char **text, my_tree *tree, tree_node *node, char *buffer);
 
 //akinator_write.cpp
 
+//! @param[in] tree Pointer to tree.
+//! @brief Saves database.
 void save_database(my_tree *tree);
 
+//! @param[in] node Node to save.
+//! @param[in] dest File to save to.
 void save_node(tree_node *node, FILE *dest);
 
 #endif
--- tree.h
+++ tree.h
@@ -19,6 +19,13 @@ enum poison
     SIZE_POSION = -1,
 };
 
+enum tree_visitor_modes
+{
+    PRE_ORDER,
+    IN_ORDER,
+    POST_ORDER,
+};
+
 typedef char tree_elmt[MAX_STR_LEN + 1];
 
 typedef struct
@@ -39,15 +46,34 @@ typedef struct
     tree_node *data;
 } my_tree;
 
+//! @param[in] dest Uninitialised tree.
+//! @param[in] capacity Tree capacity.
+//! @param[in] root_val Root value.
+//! @param[out] dest Initialised tree.
+//! @return Returns pointer to root.
+//! @brief Initialises tree.
 tree_node* tree_ctor(my_tree *dest, ssize_t capacity, const tree_elmt root_val);
 
+//! @param[in] tree Pointer to tree.
+//! @param[in] val Value to add.
+//! @return Returns pointer to new node.
+//! @brief Creates node.
 tree_node* node_ctor(my_tree *tree, const tree_elmt val);
 
+//! @param[in] tree Pointer to tree.
+//! @brief Destroys tree.
 void tree_dtor(my_tree *tree);
 
+//! @param[in] tree Pointer to tree.
+//! @brief Creates tree dump.
 void tree_dump(my_tree *tree);
 
+//! @param[in] node Pointer to tree node.
+//! @param[in] gr_viz File to print graphviz code to.
+//! @brief Prints graphviz code to file.
 void print_branch(tree_node *node, FILE *gr_viz);
+
+//void tree_visitor(tree_node *node, const char mode, );
 
 #endif
 
